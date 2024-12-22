4c9d1cecbf388065cda14984e501248f9b18e84e
exa31 <bloodsuker18@gmail.com>
2024-12-22 20:20:26 +0700

    fix: redeploy host jadi url deploy

--- docs/docs.go
+++ docs/docs.go
@@ -792,7 +792,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "quiz-sanber-production.up.railway.app",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "Quiz API Sanbercode",
